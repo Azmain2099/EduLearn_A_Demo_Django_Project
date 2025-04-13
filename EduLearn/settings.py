69155ca1e63c2b185062fe890bddbfdde742262c
Azmain Fayek <fayekazmain@yahoo.com>
2025-04-13 19:06:37 +0600

    Update Commit-02

--- EduLearn/settings.py
+++ EduLearn/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+CSRF_TRUSTED_ORIGINS= ["web-production-96794.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
